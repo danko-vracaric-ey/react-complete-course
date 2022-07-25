dda9bea5f42d51e83949babc6a5356e568f77253
Danko Vracaric <Danko.Vracaric@rs.ey.com>
2022-07-25 16:53:13 +0200

    useMemo

---
+++ react-behind-the-scenes/src/App.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
+import DemoList from "./components/UI/Button/Demo/DemoList";
+function App() {
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New   Title");
+  }, []);
+  // const [showParagraph, setShowParagraph] = useState(false);
+  // const [allowToggle, setAllowToggle] = useState(false);
+  // console.log("APP RUNNING");
+  // const toggleParagraphHandler = useCallback(() => {
+  //   if (allowToggle) {
+  //     setShowParagraph((prev) => !prev);
+  //   }
+  // }, [allowToggle]);
+
+  // const allowToggleHandler = () => {
+  //   if (allowToggle) {
+  //   }
+
+  //   setAllowToggle(true);
+  // }; // this only allowstoggling for other button
+
+  // we can tell react, only rexecute <DemoOutput. Funct only if props changes for e.g
+  return (
+    <div className="app">
+      {/* <DemoOutput show={showParagraph} /> */}
+      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]}></DemoList>
+      {/* <Button onClick={allowToggleHandler}>AllowToggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle</Button> */}
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+    </div>
+  );
+}
+
+export default App;
