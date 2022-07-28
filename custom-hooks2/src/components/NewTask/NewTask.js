03ca9483a8858f842f79022d80fc044918562b22
Danko Vracaric <Danko.Vracaric@rs.ey.com>
2022-07-28 10:26:24 +0200

    managinguseFetchPOST

--- custom-hooks2/src/components/NewTask/NewTask.js
+++ custom-hooks2/src/components/NewTask/NewTask.js
@@ -1,41 +1,30 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useFetch from "../hooks/use-fetch";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useFetch();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText }; // we don't have acces of tasText here, we can either put all function inside handler or ,expect it as an argument
+    props.onAddTask(createdTask); // we can bind the function when we pass it as an second argument fo sendTaskRequest, we can preconfigure a function
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-664f9-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: "https://react-http-664f9-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        body: JSON.stringify({ text: taskText }),
+        headers: { "Content-Type": "application/json" },
+      },
+      createTask.bind(null, taskText) // first argument, set this, here don't matter but second argument will be the first argument received by createTaskFunction
+    );
   };
 
   return (
